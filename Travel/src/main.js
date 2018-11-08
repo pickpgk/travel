ba2f031c25145de8c7418d49cb865f514e7ac4d7
pickpgk <610873384@qq.com>
2018-11-08 17:58:32 +0800

    project init

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+/*  三百毫秒点击延迟问题，在使用click事件时，会延迟300毫秒  */
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+/*  一像素边框解决方案  scale放大缩小来解决  */
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+/*  将fastclick自带的方法绑定到body上，   三步1安装2引入3绑定  */
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
