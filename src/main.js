82f12cd071840110f5bb3d5b3e08ff7f2458ba20
pickpgk <610873384@qq.com>
2018-11-10 15:38:06 +0800

    change

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import App from './App'
 import router from './router'
 /*  三百毫秒点击延迟问题，在使用click事件时，会延迟300毫秒  */
 import fastClick from 'fastclick'
+// 轮播插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 /*  一像素边框解决方案  scale放大缩小来解决  */
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 /*  将fastclick自带的方法绑定到body上，   三步1安装2引入3绑定  */
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  // data: function() {这是es5的写法}
+  // es6的写法
+  data () {
+    return {
+      swiperOption: {
+        pagination: 'swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/1b/4143c7e0bfe78d02.jpg_750x200_5a220f6e.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background-color: #eee
+    .swiper-img
+      width: 100%
+</style>
