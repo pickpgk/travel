e62a7eae8055bee89fb6ad9626afe25aba1de15f
pickpgk <610873384@qq.com>
2018-11-14 23:25:01 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    //垂直居中
+    display: flex
+    flex-direction: column
+    justify-content: center
+    //垂直居中
+    position: absolute
+    right: 0
+    top: 1.58rem
+    bottom: 0
+    width: 0.4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
