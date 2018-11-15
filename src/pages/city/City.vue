2ee23fdbc16bd56f43b670b3117ae839a5e1b503
pickpgk <610873384@qq.com>
2018-11-15 12:03:48 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -19,6 +20,33 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  // 动态数据渲染
+  data () {
+    return {
+      // 城市名字
+      cities: {},
+      // 热门城市
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      // 在config文件夹里的index.js配置过，当网址是local时，自动换成static/mock下的json
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
